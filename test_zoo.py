5809c922eda2c788472c002626153d53e60d3a99
nice1221 <132154474+nice1221@users.noreply.github.com>
2024-11-11 12:21:09 +0700

    Add files via upload

--- test_zoo.py
+++ test_zoo.py
@@ -9,6 +9,16 @@ class TestZoo(unittest.TestCase):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
        
     # Add your additional test cases here.
+    def test_invalid_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "invalid")
+    def test__teen_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(6), 50)
+    def test_student_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(15), 100)
+    def test_people_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(40), 150)
+    def test_elderly_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
 
 if __name__ == '__main__':
     unittest.main()
